58043c1049cc4f719d41f4abb224021d250373cf
Prakash Kumar <tecnocrates006@gmail.com>
2025-02-26 23:24:37 +0530

    add fevicon

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,17 @@ import ApiResponse from "./utils/apiResponse";
 import { errorHandler } from "./utils/errorHandler";
 import { notFoundHandler } from "./utils/notFoundHadnler";
 import referralRoute from "./routes/referral.route";
+import path = require("path");
 
 dotenv.config();
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(new ApiResponse(200, "Welcome to the API", null));
+  res.send(new ApiResponse(200, " Accredian-Backend-task", null));
 });
 
 app.use("/api/referrals", referralRoute);
